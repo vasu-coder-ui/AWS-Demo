85321c01fe146490debf11a0884f58702e13da7e
vasu-coder-ui <agvasupydev123@gmail.com>
2026-03-05 12:22:30 +0530

    Update urls.py

--- Ag/urls.py
+++ Ag/urls.py
@@ -25,6 +25,4 @@ urlpatterns = [
      path('', views.home),
     path('list/', views.product_list, name='product_list'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
